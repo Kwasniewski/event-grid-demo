0b27aa79d44d5014270c2025880511428330ecae
Joey Kwasniewski <kwasniewski@gmail.com>
2025-09-17 11:25:17 -0700

    cleanup

--- terraform/envs/test/main.tf
+++ terraform/envs/test/main.tf
@@ -18,8 +18,3 @@ module "event_grid" {
   region              = var.region
   resource_group_name = var.resource_group_name
 }
-
-import {
-  to = module.event_grid.azurerm_eventgrid_namespace.namespace
-  id = "/subscriptions/e88fa6f2-4a57-4402-8c6f-861bf76a1cd3/resourceGroups/MC_production-tb_production-tb_eastus/providers/Microsoft.EventGrid/namespaces/kwasniewski-event-grid-test"
-}
